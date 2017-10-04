2eb6d1406c340d122b8331f34c4991dc6fe3d355
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2017-10-04 14:00:05 +0200

    Versie 0.1

--- Memory/random.cs
+++ Memory/random.cs
@@ -9,9 +9,16 @@ namespace Memory
     class random
     {
 
-        public static void Rand()
+        public static void Rand(int []shuf)
         {
-
+            int[] shuf = new int[16] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+            for (int t = 0; t < shuf.Length; t++)
+            {
+                int tmp = shuf[t];
+                int r = Random.Range(t, shuf.Length);
+                shuf[t] = shuf[r];
+                shuf[r] = tmp;
+            }
         }
 
     }
