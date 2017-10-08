8af9691a9e778f16b854fee03c54e9ec5cb2f4eb
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2017-10-08 14:41:40 +0200

    Add files via upload

--- Memory/random.cs
+++ Memory/random.cs
@@ -8,5 +8,19 @@ namespace Memory
 {
     class random
     {
+
+        public static void Rand()
+        {
+            string[] shuf = new string[16] {"1", "2", "3", "4", "5", "6", "7", "8", "1", "2", "3", "4", "5", "6", "7", "8"};
+            Random rnd = new Random();
+            for (int t = 0; t < shuf.Length; t++)
+            {
+                string tmp = shuf[t];
+                int r = rnd.Next(t, shuf.Length);
+                shuf[t] = shuf[r];
+                shuf[r] = tmp;
+            }
+        }
+
     }
 }
